b8b3fef6e473738703cf66da8f1f644832e69731
yonny <ycnovac@unal.edu.co>
2021-10-10 18:33:05 -0500

    adding solution similarity but with performace issues

--- first-try-similarity-array.go
+++ first-try-similarity-array.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 )
 
+
 func getCount( a []int ) map[int]int{
 	count_a := make(map[int] int, 0)
 	for _, value := range a {
@@ -17,13 +18,34 @@ func getCount( a []int ) map[int]int{
 	return count_a
 }
 
+func swap( input []int, i int, j int) []int {
+	input[i], input[j] = input[j], input[i]
+	return  input
+}
+
 func areSimilar(a []int, b []int) bool {
-	count_a := getCount( a )
+    count_a := getCount( a )
 	count_b := getCount( b )
 	res := reflect.DeepEqual(count_a, count_b)
-	return res
+	temp := b
+	if( res ){
+		for i:= 0 ; i < len(a); i++ {
+			for j:= 0 ; j < len(a); j++ {
+
+				temp = swap(temp, i, j)
+				isEqual := reflect.DeepEqual(temp, a)
+
+				if( isEqual ) {
+					return isEqual;
+					break
+				}
+				temp = swap(temp, i, j)
+			}
+		}
+	}
+	return false
 }
 
 func main() {
-	fmt.Println(areSimilar([]int{1, 2, 2 },[]int{2, 1, 1}))
+	fmt.Println(areSimilar([]int{2, 3, 1},[]int{1, 3, 2}))
 }
