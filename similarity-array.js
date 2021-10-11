5dd6c0669586e954a2a951a5807823f02e7cbb8b
yonny <ycnovac@unal.edu.co>
2021-10-10 21:26:09 -0500

    similarity with node

---
+++ similarity-array.js
@@ -0,0 +1,22 @@
+function areSimilar(a, b) {
+    if(a.toString() == b.toString()){
+        return true;
+    }
+    let c = []
+    let d = []
+    a.map((value,idx)=>{
+        if(value != b[idx]){
+            console.log(b[idx]);
+            c.push(value);
+            d.push(b[idx]);
+        }
+    })
+
+    d = d.reverse();
+    if(c.length ===2 && (c.toString() == d.toString())){
+        return true
+    }
+    return false;
+}
+
+console.log(areSimilar([1, 2, 3],[2, 1, 3]))
