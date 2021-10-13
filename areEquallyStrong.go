7d7a335cd8fce659ff199f0f8c06ef4f8f27c2ff
yonny <ycnovac@unal.edu.co>
2021-10-13 14:58:48 -0500

    adding are equaly strong

---
+++ areEquallyStrong.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func areEquallyStrong(yourLeft int, yourRight int, friendsLeft int, friendsRight int) bool {
+	yourLeftT := float64(yourLeft)
+	yourRightT := float64(yourRight)
+	friendsLeftT := float64(friendsLeft)
+	friendsRightT := float64(friendsRight)
+	return (math.Min(yourLeftT, yourRightT ) == math.Min(friendsLeftT, friendsRightT) && math.Max(yourLeftT, yourRightT ) == math.Max(friendsLeftT, friendsRightT) )
+}
+
+
+func main() {
+ fmt.Println(areEquallyStrong(10, 15, 15, 10))
+}
