72a2e089e40f6b059eeb2eca3f99c75d2870ebc8
yonny <ycnovac@unal.edu.co>
2021-10-15 18:46:59 -0500

    adding chanllenge for ip4 valid

---
+++ isValidIpAddress.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func isIPv4Address(inputString string) bool {
+  
+	result := strings.Split(inputString, ".")
+	for _, v := range result {
+		start_with_zero := strings.HasPrefix(v, "0")
+		transform, err := strconv.Atoi(v)
+		if ( transform > 255 || len(result) != 4 || err != nil || (start_with_zero && v != "0") ) {
+
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isIPv4Address("0.254.255.0"))
+}
