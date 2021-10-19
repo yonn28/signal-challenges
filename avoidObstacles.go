452b6a91a85b228d82e2f49fe646c5518b98e888
yonny <ycnovac@unal.edu.co>
2021-10-18 19:47:54 -0500

    adding avoid obstacles

---
+++ avoidObstacles.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func isDivisible(inputArray []int, divisor int) bool { 
+	for i := 0; i < len(inputArray); i++ {
+		if inputArray[i] % divisor == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func avoidObstacles(inputArray []int) int {
+	isValidStep := false
+	stepInit := 2
+	for !isValidStep {
+		isValidStep = isDivisible(inputArray, stepInit)
+		if !isValidStep {
+			stepInit++
+		}
+	}
+	return stepInit
+}
+
+
+func main() {
+	fmt.Println(avoidObstacles([]int{19, 32, 11, 23}))
+	fmt.Println(avoidObstacles([]int{5, 3, 6, 7, 9}))
+	fmt.Println(avoidObstacles([]int{2, 3}))
+}
