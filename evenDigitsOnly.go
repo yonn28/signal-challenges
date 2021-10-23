ecd25d0989219fb65262b839cada4b8dea013f19
yonny <ycnovac@unal.edu.co>
2021-10-22 20:10:59 -0500

    Adding even digits only

---
+++ evenDigitsOnly.go
@@ -0,0 +1,23 @@
+package main
+
+import ("fmt"
+	"strings"
+	"strconv"
+)
+
+func evenDigitsOnly(n int) bool {
+	number_string := strconv.Itoa(n)
+	splited:= strings.Split(number_string,"")
+	isEven := true
+	for _, value := range splited {
+		elemnt, err := strconv.Atoi(value)
+		if elemnt % 2 != 0 && err == nil {
+			isEven = false
+		}
+	}
+	return isEven
+}
+
+func main() {
+	fmt.Println(evenDigitsOnly(248622))
+}
